260a164c4108cc67923b217288c47cf76610e685
Don Marti <dmarti@zgp.org>
2017-07-16 08:20:01 -0700

    extend ad blocker warning

--- src/code/test.js
+++ src/code/test.js
@@ -1,6 +1,6 @@
 var failMessage = '<div class="warning"> <p><b>Third-party tracking detected:</b> Your browser is vulnerable to third-party tracking. Please install or configure <a href="/protection/">tracking protection</a> to help protect yourself and the web sites you use.<p> <p><a target="_blank" class="button" href="/tracking-protection/">Get tracking protection</a></p>';
 
-var abWarning = '<p id="abpwarning"><b>This browser appears to be running an ad blocker that is not providing tracking protection. You need <a href="/protection/">tracking protection</a></b>.</p>';
+var abWarning = '<p id="abpwarning"><b>This browser appears to be running an ad blocker that is not providing tracking protection. Some ad blockers operate a deceptive paid whitelisting scheme called <q>Acceptable Ads</q> that allows tracking by some problem companies. To get protection, you also need a <a href="/protection/">tracking protection tool</a></b>. (You do not need to uninstall your ad blocker to use tracking protection.)</p>';
 
 var passMessage = '<p><b>Third-party tracking not detected.</b> This browser is safe from some common kinds of tracking.</p> <p>Thank you for helping to protect the web sites you use from data leakage, fraud, and malware.</p> <p>(This test covers some of the basics, but no system can test for every kind of data collection.  For more info on protecting yourself online, please see <a href="https://ssd.eff.org/en/playlist/want-security-starter-pack">Want a security starter pack?</a> from the Electronic Frontier Foundation.)</p>';
 
